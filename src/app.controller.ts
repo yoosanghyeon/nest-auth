b1082bfcc2a2b2e9f28dfd8491732de3e08ceb72
YooSangHyeon <yoosanghyeon1@gmail.com>
2022-12-19 09:24:37 +0900

    func/tested-auth

---
+++ src/app.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Request, Get, Post, UseGuards } from '@nestjs/common';
+import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './auth/local-auth.guard'
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
+  }
+
+}
